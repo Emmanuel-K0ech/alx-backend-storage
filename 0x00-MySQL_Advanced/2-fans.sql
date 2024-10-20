3066df972fcf158e0bc5f41554a19b376766556c
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-20 23:29:19 +0300

    Task 2: Importing and dumping sql file and creating a script to select two fields and order by number

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,5 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 -- script can be executed on any database
-
+SELECT origin, fans AS nb_fans
+FROM metal_bands
+ORDER BY nb_fans DESC;
